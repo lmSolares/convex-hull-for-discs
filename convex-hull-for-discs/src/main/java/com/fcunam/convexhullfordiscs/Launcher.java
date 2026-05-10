b49181cbc7512a19122ba4639cef85d059dd3f57
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-05-10 10:25:08 -0600

    Application launcher

---
+++ convex-hull-for-discs/src/main/java/com/fcunam/convexhullfordiscs/Launcher.java
@@ -0,0 +1,8 @@
+package com.fcunam.convexhullfordiscs;
+import javafx.application.Application;
+
+public class Launcher {
+    static void main(String[] args) {
+        Application.launch(ConvexHullApp.class, args);
+    }
+}
