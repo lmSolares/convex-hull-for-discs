a448a70e4fdb2e379303ac3995c8a7a5b385c294
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-05-10 10:21:45 -0600

    feat(operations): The operations to calculate the common support line between two disks are implemented

--- convex-hull-for-discs/src/main/java/com/fcunam/convexhullfordiscs/model/math/geometry/GeometryUtils.java
+++ convex-hull-for-discs/src/main/java/com/fcunam/convexhullfordiscs/model/math/geometry/GeometryUtils.java
@@ -1,16 +1,22 @@
-package com.fcunam.convexhullfordiscs.math.geometry;
+package com.fcunam.convexhullfordiscs.model.math.geometry;
 
-import com.fcunam.convexhullfordiscs.math.structs.Disc;
-import com.fcunam.convexhullfordiscs.math.structs.Line;
-import com.fcunam.convexhullfordiscs.math.structs.OrderedPair;
+import com.fcunam.convexhullfordiscs.model.math.structs.Disc;
+import com.fcunam.convexhullfordiscs.model.math.structs.Line;
+import com.fcunam.convexhullfordiscs.model.math.structs.OrderedPair;
 import org.jetbrains.annotations.NotNull;
 
 /**
- *
+ * GeometryUtils
+ * Class with the main geometric operations
  * */
 public class GeometryUtils {
 
     /**
+     * Determine the support line that two discs have in common, that is, a line that leaves both discs on only one side.
+     *
+     * @param disc1 first disc
+     * @param disc2 second disc
+     * @return Common support line
      * */
     public static Line calculateCommonSupportLine(@NotNull Disc disc1, @NotNull Disc disc2) {
 
@@ -36,18 +42,20 @@ public class GeometryUtils {
     }
 
     /**
-     * @param point1
-     * @param point2
-     * @return
+     * Determine the angle from point p1 to p2
+     * @param point1 First point
+     * @param point2 Second point
+     * @return Angle between both points
      * */
     public static double calculateAngle(@NotNull OrderedPair point1, @NotNull OrderedPair point2) {
         return Math.atan2(point2.y() - point1.y(), point2.x() - point1.x());
     }
 
     /**
-     * @param point1
-     * @param point2
-     * @return
+     * Determine the distance between two points
+     * @param point1 First point
+     * @param point2 Second point
+     * @return Distance between two points
      * */
     public static double calculateDistance(@NotNull OrderedPair point1, @NotNull OrderedPair point2) {
         double x = point2.x() - point1.x();
